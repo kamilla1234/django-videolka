a4c7c3d93c08dbfd4ae17998a373342af0580e5b
evelina <evelina.n.05@mail.ru>
2024-09-17 08:58:30 +0300

    Created 'video' app

---
+++ videolka/video/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Video(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    thumbail = models.ImageField(upload_to="videos/thumbnails")
+    source = models.FileField(upload_to="videos")
