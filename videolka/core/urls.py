1b3691656a1f8aa005344ff7fc4c795824f38e98
evelina <evelina.n.05@mail.ru>
2024-09-17 10:14:21 +0300

    Add video model to admin

--- videolka/core/urls.py
+++ videolka/core/urls.py
@@ -1,8 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
 
 urlpatterns = [
     path('', include('main.urls')),
     path('video/', include('video.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- videolka/video/views.py
+++ videolka/video/views.py
@@ -1,5 +1,10 @@
+from lib2to3.fixes.fix_input import context
+
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from . import models
 
 def video_page(request:HttpRequest) -> HttpResponse:
-    return HttpResponse('This is video')
+    video = models.Video.objects.get(pk=1)
+    context = {'video': video}
+    return render(request, 'video/video.html', context)
