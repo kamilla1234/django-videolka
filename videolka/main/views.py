980497c18f2b6b188ca483a03f158346c2a08ef1
evelina <evelina.n.05@mail.ru>
2024-09-19 09:55:15 +0300

    update

--- videolka/main/views.py
+++ videolka/main/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from video import models
 
 def home_page(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('Универ - Старая общага - Смотреть онлайн')
+    videos = models.Video.objects.all()
+    context = {'videos': videos}
+
+    return render(request, 'main/index.html', context)
