f7132dc143766102bb61f2c434182bf8edfea704
evelina <evelina.n.05@mail.ru>
2024-09-20 09:44:44 +0300

    Added login app

--- videolka/core/urls.py
+++ videolka/core/urls.py
@@ -7,6 +7,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('main.urls')),
     path('video/', include('video.urls')),
+    path('', include('login.urls')),
     path('admin/', admin.site.urls),
 ]
 
